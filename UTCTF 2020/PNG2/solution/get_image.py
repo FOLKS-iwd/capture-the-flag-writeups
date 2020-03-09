07c9a18ef72869edcaea95cebaa0460ed2797ffb
Burgos1337 <57829161+Burgos1337@users.noreply.github.com>
2020-03-09 11:25:02 +0300

    bof and .PNG2 writeups added

---
+++ UTCTF 2020/PNG2/solution/get_image.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import struct
+import sys
+from PIL import Image
+
+if len(sys.argv) != 2:
+	print("[+] Usage : ./{sys.argv[0]} <filename>")
+	exit(1)
+
+with open(sys.argv[1] , "rb") as f:
+	data = f.read()
+
+assert data[:4] == b'PNG2'
+
+width = struct.unpack(">H" , data[10:12])[0]
+height = struct.unpack(">H" , data[19:21])[0]
+
+img = Image.frombytes("RGB" , (width , height) , data[21:])
+img.save("check.png" , "PNG")
