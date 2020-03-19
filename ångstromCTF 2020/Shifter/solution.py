e40aad0d22edbc801c583c406a038dff8e9f1d55
Burgos1337 <57829161+Burgos1337@users.noreply.github.com>
2020-03-19 15:15:30 +0300

    Shifter writeup modified

--- ångstromCTF 2020/Shifter/solution.py
+++ ångstromCTF 2020/Shifter/solution.py
@@ -1,7 +1,7 @@
 from pwn import *
 from functools import reduce
 
-fib = lambda n:reduce(lambda x,n:[x[1],x[0]+x[1]], range(n),[0,1])[0]
+fib = lambda n:reduce(lambda x, n:[x[1], x[0] + x[1]], range(n), [0, 1])[0]
 
 def debug(txt, res, n):
     log.info("Text {txt} rotated with key {n}  --> {res}")
@@ -9,7 +9,7 @@ def debug(txt, res, n):
 def encrypt(text,s):
     result = ""
     for i in range(len(text)):
-        result += chr((ord(text[i]) + s-65) % 26 + 65)
+        result += chr((ord(text[i]) + s - 65) % 26 + 65)
     return result
 
 con = remote("52.207.14.64", 20300)
